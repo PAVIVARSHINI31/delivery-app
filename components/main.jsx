8137e797c84fcf31000e6b2b6d80a074ca7ed24a
PAVIVARSHINI31 <pavivarshini11@gmail.com>
2026-06-27 14:45:08 +0530

    Add files via upload

---
+++ components/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import appStore from '../Constants/appStore'
+import { Provider } from 'react-redux'
+
+
+import App from "./App";
+
+createRoot(document.getElementById('root')).render(
+  
+    <Provider store={appStore}>
+      <App />
+    </Provider>
+    
+
+)
+
